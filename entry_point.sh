51265601a5ecd6fc8425e9a8dce9d771fd2d5a1e
Yuval Turgeman <yturgema@redhat.com>
2025-05-20 17:20:12 -0400

    Use values from env
    Support store config and db values from env
    
    Signed-off-by: Yuval Turgeman <yturgema@redhat.com>

--- entry_point.sh
+++ entry_point.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "entry point stasting"
+yq eval ".project = \"$FEAST_PROJECT_NAME\"" -i /app/feature_repo/feature_store.yaml
+yq eval ".registry.path = \"$FEAST_REGISTRY_URL\"" -i /app/feature_repo/feature_store.yaml
+yq eval ".online_store.host = \"$DB_HOST\"" -i /app/feature_repo/feature_store.yaml
+yq eval ".online_store.port = \"$DB_PORT\"" -i /app/feature_repo/feature_store.yaml
+yq eval ".online_store.user = \"$DB_USER\"" -i /app/feature_repo/feature_store.yaml
 yq eval ".online_store.password = \"$DB_PASSWORD\"" -i /app/feature_repo/feature_store.yaml
+yq eval ".online_store.database = \"$DB_NAME\"" -i /app/feature_repo/feature_store.yaml
 cat /app/feature_repo/feature_store.yaml
 echo "entry point ending"
