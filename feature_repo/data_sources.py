b961fceeed1807ae06de88c54e6c9b2c4d7e5703
Itay Katav <ikatav@redhat.com>
2025-06-10 15:20:43 +0300

    remove redundent code

--- feature_repo/data_sources.py
+++ feature_repo/data_sources.py
@@ -1,9 +1,6 @@
 from feast import FileSource, PushSource
 from feast.data_format import ParquetFormat
 import os
-from feast.infra.offline_stores.contrib.postgres_offline_store.postgres_source import (
-    PostgreSQLSource,
-)
 
 feast_path = 'feature_repo'
 data_path = 'data'
@@ -63,31 +60,3 @@ item_textual_features_embed_push_source = PushSource(
     name='item_textual_features_embed',
     batch_source=item_textual_features_embed_dummy_source
 )
-
-# interaction_stream_source = KafkaSource(
-#     name="intercation_stream_source",
-#     kafka_bootstrap_servers='xray-cluster-kafka-bootstrap.jary-feast-example.svc.cluster.local:9092',
-#     topic="interactions",
-#     timestamp_field="timestamp",
-#     batch_source=interactions_source,
-#     message_format=JsonFormat(
-#         schema_json="user_id integer, item_id integer, timestamp timestamp, interaction_type string, rating integer, quantity integer"
-#     ),
-#     watermark_delay_threshold=timedelta(minutes=5),
-# )
-
-
-# interaction_stream_source = PostgreSQLSource(
-#     name="interaction_stream_source",
-#     query="SELECT * FROM stream_positive_interaction",
-#     # timestamp_field="event_timestamp",
-#     timestamp_field="timestamp",
-#     # created_timestamp_column="created",
-# )
-# interaction_stream_source = PostgreSQLSource(
-#     name="interaction_stream_source",
-#     query="SELECT * FROM stream_negetive_interaction",
-#     # timestamp_field="event_timestamp",
-#     timestamp_field="timestamp",
-#     # created_timestamp_column="created",
-# )
